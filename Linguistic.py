70bb7ed55357af3505ae60f9e44b6f95f56326cb
lankovova <lanko.vova@gmail.com>
2018-10-09 22:30:28 +0300

    added merge DFs function

--- Linguistic.py
+++ Linguistic.py
@@ -9,6 +9,7 @@ LETTERS_AMOUNT = len(LETTERS_UPPERCASE)
 MAX_MOVEMENT_VALUE = 500
 MOVEMENT_STEP = MAX_MOVEMENT_VALUE // LETTERS_AMOUNT
 
+
 def getMovementLetter(value):
 	letterIndex = abs(value) // MOVEMENT_STEP
 
@@ -16,6 +17,16 @@ def getMovementLetter(value):
 		return LETTERS_UPPERCASE[letterIndex if letterIndex < len(LETTERS_UPPERCASE) else -1]
 	else:
 		return LETTERS_LOWERCASE[letterIndex if letterIndex < len(LETTERS_LOWERCASE) else -1]
+
+
+def mergeTransitionsDFs(left, right):
+	return (left + right) / 2
+
+
+# TODO
+def isTheSamePerson(sourceDF, toCheckDF):
+	pass
+
 
 def linguistic(timeline):
 	# Get letters timelines
@@ -39,6 +50,6 @@ def linguistic(timeline):
 	# Convert transitions amount into transition chance percentage
 	transitionsPD = transitionsPD.apply(lambda x: x / x.sum() if x.any() else np.zeros(x.size), axis=1)
 
-	print(transitionsPD.head())
+	# print(transitionsPD.head())
 
 	return { "transitions": transitionsPD.reset_index().to_json(orient="records") }
