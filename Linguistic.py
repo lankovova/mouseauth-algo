389e8477937f0c5f4dce5a3c997a9d79ef23cea9
lankovova <lanko.vova@gmail.com>
2018-10-20 21:55:24 +0300

    refactoring

--- Linguistic.py
+++ Linguistic.py
@@ -1,17 +1,7 @@
 import numpy as np
 import pandas as pd
-import string
 import datasource as DS
-
-LETTERS_UPPERCASE = list(string.ascii_uppercase)
-LETTERS_LOWERCASE = list(string.ascii_lowercase)
-LETTERS_AMOUNT = len(LETTERS_UPPERCASE)
-
-LETTERS_TRANSITION_ALLOWABLE_ERROR = 0.05
-GRAMMAR_ALLOWABLE_ERROR = 0.02
-
-FULLNESS_TRAININGS_AMOUNT = 20
-
+import constants as c
 
 def getTimelineDeltas(timeline):
 	timelineDeltas = []
@@ -23,54 +13,25 @@ def getTimelineDeltas(timeline):
 	return timelineDeltas
 
 
-def getLetterStep(timelineDeltas):
-	return max(timelineDeltas) // LETTERS_AMOUNT
-
-
 def getLetter(value, step):
 	letterIndex = abs(value) // step
 	if value >= 0:
-		return LETTERS_UPPERCASE[letterIndex if letterIndex < len(LETTERS_UPPERCASE) else -1]
+		return c.LETTERS_UPPERCASE[letterIndex if letterIndex < len(c.LETTERS_UPPERCASE) else -1]
 	else:
-		return LETTERS_LOWERCASE[letterIndex if letterIndex < len(LETTERS_LOWERCASE) else -1]
-
-
-def mergeGrammars(left, right):
-	return (left + right) / 2
-
-
-def mergeDictOfGrammars(leftDict, rightDict):
-	return { k: mergeGrammars(v, rightDict[k]) for k, v in leftDict.items() }
-
-
-def getGrammarError(srcGrammar, grammarsToCheck):
-	grammarLettersHits = abs(srcGrammar - grammarsToCheck) <= LETTERS_TRANSITION_ALLOWABLE_ERROR
-	elementsAmount = len(srcGrammar.index) * len(srcGrammar.columns)
-
-	return abs(elementsAmount - grammarLettersHits.sum().sum()) / elementsAmount
-
-
-def isGrammarsSimilar(srcGrammars, grammarsToCheck):
-	grammarsErrors = { k: getGrammarError(v, grammarsToCheck[k]) for k, v in srcGrammars.items() }
-	print(grammarsErrors)
-
-	# Check if grammars errors is in OK range
-	isGrammarsSimilarDict = { k: v <= GRAMMAR_ALLOWABLE_ERROR for k, v in grammarsErrors.items() }
-
-	return True # FIXME:
+		return c.LETTERS_LOWERCASE[letterIndex if letterIndex < len(c.LETTERS_LOWERCASE) else -1]
 
 
 def formGrammar(timeline):
 	timelineDeltas = getTimelineDeltas(timeline)
-	letterStep = getLetterStep(timelineDeltas)
+	letterStep = max(timelineDeltas) // c.LETTERS_AMOUNT
 
 	lettersChain = [getLetter(timepointDelta, letterStep) for timepointDelta in timelineDeltas]
 
 	# Initialize empty matrix
 	grammar = pd.DataFrame(
-		np.zeros((2 * LETTERS_AMOUNT, 2 * LETTERS_AMOUNT), dtype=int),
-		index=LETTERS_UPPERCASE + LETTERS_LOWERCASE,
-		columns=LETTERS_UPPERCASE + LETTERS_LOWERCASE
+		np.zeros((2 * c.LETTERS_AMOUNT, 2 * c.LETTERS_AMOUNT), dtype=int),
+		index=c.LETTERS_UPPERCASE + c.LETTERS_LOWERCASE,
+		columns=c.LETTERS_UPPERCASE + c.LETTERS_LOWERCASE
 	)
 
 	# Add transitions between letters amount
@@ -88,21 +49,45 @@ def formGrammars(timelines):
 	return { k: formGrammar(v) for k, v in timelines.items() }
 
 
+def mergeGrammars(left, right):
+	return (left + right) / 2
+
+
+def mergeDictOfGrammars(leftDict, rightDict):
+	return { k: mergeGrammars(v, rightDict[k]) for k, v in leftDict.items() }
+
+
+def getGrammarError(srcGrammar, grammarsToCheck):
+	grammarLettersHits = abs(srcGrammar - grammarsToCheck) <= c.LETTERS_TRANSITION_ALLOWABLE_ERROR
+	elementsAmount = len(srcGrammar.index) * len(srcGrammar.columns)
+
+	return abs(elementsAmount - grammarLettersHits.sum().sum()) / elementsAmount
+
+
+def isGrammarsSimilar(srcGrammars, grammarsToCheck):
+	grammarsErrors = { k: getGrammarError(v, grammarsToCheck[k]) for k, v in srcGrammars.items() }
+	print(grammarsErrors)
+
+	# Check if grammars errors is in OK range
+	isGrammarsSimilarDict = { k: v <= c.GRAMMAR_ALLOWABLE_ERROR for k, v in grammarsErrors.items() }
+
+	return True # FIXME:
+
+
 def linguistic(timelines, userID):
-	grammars = formGrammars(timelines)
+	currentGrammars = formGrammars(timelines)
 
 	# Get user data
-	userData = DS.getUserData(userID)
+	userSourceData = DS.getUserData(userID)
 
-	if not userData:
+	if not userSourceData:
 		# New user
-		DS.addNewUser(userID, grammars)
-	elif userData["trainings"] < FULLNESS_TRAININGS_AMOUNT:
-		mergedGrammars = mergeDictOfGrammars(userData["grammars"], grammars)
+		DS.addNewUser(userID, currentGrammars)
+	elif userSourceData["trainings"] < c.FULLNESS_TRAININGS_AMOUNT:
+		mergedGrammars = mergeDictOfGrammars(userSourceData["grammars"], currentGrammars)
 		DS.trainUser(userID, mergedGrammars)
 	else:
 		# Compare grammars
-		compareResults = isGrammarsSimilar(userData["grammars"], grammars)
-		return compareResults
+		return isGrammarsSimilar(userSourceData["grammars"], currentGrammars)
 
 	return True
